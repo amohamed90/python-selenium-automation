3862783e5a6b6c2966c0a8bb3507e5d68375f82d
Alaa M <alaagmohamed90@gmail.com>
2025-05-22 20:34:34 -0700

    amazon locators, target sign-in and search scripts

--- amazon_script.py
+++ amazon_script.py
@@ -13,32 +13,65 @@ driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
 # open the url
-driver.get('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0')
+# driver.get('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0')
+#
+# # Amazon logo
+# # Email field
+# # Continue button
+# # Conditions of use link
+# # Privacy Notice link
+# # Need help link
+# # Forgot your password link
+# # Other issues with Sign-In link
+# # Create your Amazon account button
+#
+# driver.find_element(By.XPATH, "//i[@role='presentation']")
+# driver.find_element(By.XPATH, "//input[@id='ap_email']")
+# driver.find_element(By.XPATH, "//input[@id='continue']")
+# # There is another Conditions of use link at the bottom of the page so i used the parent/child formula to get the correct one
+# driver.find_element(By.XPATH, "//div[@id='legalTextRow']//a[contains(@href, 'nodeId=508088')]")
+# # There is another Privacy Notice link at the bottom of the page so i used the parent/child formula to get the correct one
+# driver.find_element(By.XPATH, "//div[@id='legalTextRow']//a[contains(@href, 'nodeId=468496')]")
+# driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']").click()
+# driver.find_element(By.ID, 'auth-fpp-link-bottom')
+# driver.find_element(By.ID, 'ap-other-signin-issues-link')
+# driver.find_element(By.ID, 'createAccountSubmit')
+#
+# print('Found All Elements')
 
-# Amazon logo
-# Email field
-# Continue button
-# Conditions of use link
-# Privacy Notice link
-# Need help link
-# Forgot your password link
-# Other issues with Sign-In link
-# Create your Amazon account button
+####################################################################################
 
+# open url
+driver.get('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
+sleep(5)
+# locate amazon logo
 driver.find_element(By.XPATH, "//i[@role='presentation']")
-driver.find_element(By.XPATH, "//input[@id='ap_email']")
-driver.find_element(By.XPATH, "//input[@id='continue']")
-# There is another Conditions of use link at the bottom of the page so i used the parent/child formula to get the correct one
-driver.find_element(By.XPATH, "//div[@id='legalTextRow']//a[contains(@href, 'nodeId=508088')]")
-# There is another Privacy Notice link at the bottom of the page so i used the parent/child formula to get the correct one
-driver.find_element(By.XPATH, "//div[@id='legalTextRow']//a[contains(@href, 'nodeId=468496')]")
-driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']").click()
-driver.find_element(By.ID, 'auth-fpp-link-bottom')
-driver.find_element(By.ID, 'ap-other-signin-issues-link')
-driver.find_element(By.ID, 'createAccountSubmit')
 
-print('Found All Elements')
+# locate email field
+driver.find_element(By.ID, "ap_email")
 
+# locate 'continue' button
+driver.find_element(By.ID, "continue")
+
+# locate 'Conditions of Use' link
+driver.find_element(By.XPATH, "//div[@id='legalTextRow']//a[contains(@href, 'condition_of_use')]")
+
+# locate 'Privacy Notice' link
+driver.find_element(By. XPATH, "//div[@id='legalTextRow']//a[contains(@href, 'privacy_notice')]")
+
+# locate 'Need help' link
+driver.find_element(By.XPATH, '//span[@class="a-expander-prompt"]')
+
+# locate 'Forgot your password' link
+driver.find_element(By.ID, "auth-fpp-link-bottom")
+
+# locate 'Other issues with Sign-In' link
+driver.find_element(By.ID, "ap-other-signin-issues-link")
+
+# locate 'Create your Amazon account' button
+driver.find_element(By.ID, "createAccountSubmit")
+
+print('Test passed')
 
 
 
--- target_sign_in_page.py
+++ target_sign_in_page.py
@@ -12,19 +12,40 @@ service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
+# # open the url
+# driver.get('https://www.target.com/')
+#
+# # Click the signin button
+# driver.find_element(By.XPATH, "//a[@data-test='@web/AccountLink']").click()
+#
+# # Click signin from side navigation
+# driver.find_element(By.XPATH, "//button[@data-test='accountNav-signIn']").click()
+#
+# # Verify Signin page opened > “Sign into your Target account” text is shown
+# driver.find_element(By.XPATH, "//span[text() ='Sign into your Target account']")
+#
+# # Verify Signin page opened > SignIn button is shown
+# driver.find_element(By.ID, 'login')
+#
+# print('Tests passed')
+
+#####################################################################################
+
 # open the url
 driver.get('https://www.target.com/')
 
-# Click the signin button
-driver.find_element(By.XPATH, "//a[@data-test='@web/AccountLink']").click()
+# click on 'Account' button
+driver.find_element(By.ID, 'account-sign-in').click()
+sleep(5)
 
-# Click signin from side navigation
+# click on the sign-in btn from the side navigation
 driver.find_element(By.XPATH, "//button[@data-test='accountNav-signIn']").click()
+sleep(5)
 
-# Verify Signin page opened > “Sign into your Target account” text is shown
-driver.find_element(By.XPATH, "//span[text() ='Sign into your Target account']")
+# verify sign-in page opened
+driver.find_element(By.XPATH, "//h1[text()='Sign in or create account']")
 
-# Verify Signin page opened > SignIn button is shown
+# sign-in btn is shown
 driver.find_element(By.ID, 'login')
 
-print('Tests passed')
+print('test passed')
