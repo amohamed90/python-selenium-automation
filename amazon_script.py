2f44a3d41b58441fe23f2934875bd369afc74b3d
Alaa M <alaagmohamed90@gmail.com>
2025-06-20 01:53:37 -0700

    practice changes

--- amazon_script.py
+++ amazon_script.py
@@ -43,7 +43,7 @@ driver.maximize_window()
 
 # open url
 driver.get('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
-sleep(5)
+
 # locate amazon logo
 driver.find_element(By.XPATH, "//i[@role='presentation']")
 
---
+++ features/tests/product_colors.feature
@@ -0,0 +1,6 @@
+# Created by alaamohamed at 6/19/25
+Feature: # Enter feature name here
+  # Enter feature description here
+
+  Scenario: # Enter scenario name here
+    # Enter steps here
